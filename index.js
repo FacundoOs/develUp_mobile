257837aa3c28907946d2d2d11d975a3dd77a55cd
FacundoOs <osores.facundo@gmail.com>
2020-11-06 12:04:20 +0100

    testing json mock

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/";
+// axios.defaults.baseURL = "http://localhost:3000/api/";
+axios.defaults.baseURL = "https://my-json-server.typicode.com/FacundoOs/develUp_mobile/ ";
 
 registerRootComponent(App);
+
